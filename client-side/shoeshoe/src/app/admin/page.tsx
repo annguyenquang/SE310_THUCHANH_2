e9a739849bb839ba5c5d0a327cee404a6aa75ac6
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-28 12:15:44 +0700

    Add tabs

--- client-side/shoeshoe/src/app/admin/page.tsx
+++ client-side/shoeshoe/src/app/admin/page.tsx
@@ -1,21 +1,41 @@
+"use client"
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { ProductList } from "../../../components/common/ProductList";
 import Container from "@mui/material/Container";
 import Grid2 from "@mui/material/Grid2";
+import React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const AdminPage = () => {
+    const [tabIndex, setTabIndex] = React.useState(0);
+    const handleTabChange = (event: React.SyntheticEvent, newIndex: number) => {
+        setTabIndex(newIndex);
+    };
+
     return (
         <Box padding={1}>
-            <Container>
-                <Grid2 container spacing={3}>
-                    <TextField
-                        fullWidth
-                        label="Search"
-                    />
-                    <ProductList isAdmin></ProductList>
-                </Grid2>
-            </Container>
+            <Tabs value={tabIndex} onChange={handleTabChange} aria-label="admin tabs">
+                <Tab label="Product List" />
+                <Tab label="Dashboard" />
+            </Tabs>
+            {tabIndex === 0 &&
+                <Container>
+                    <Grid2 container spacing={3}>
+                        <TextField
+                            fullWidth
+                            label="Search"
+                        />
+                        <ProductList isAdmin></ProductList>
+                    </Grid2>
+                </Container>
+            }
+            {tabIndex == 1 &&
+                <Container>
+                    This is the dashboard
+                </Container>
+            }
         </Box>
     );
 }
